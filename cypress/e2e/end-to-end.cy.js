f7764e82540ca3c521e249feda04c33223d28838
Alex Mariano <alexdealm@gmail.com>
2026-04-03 08:39:38 -0300

    feat: adiciona teste E2E de cadastro e login com Faker

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -1,28 +1,32 @@
+   /// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
 describe('Testes End To End do fluxo de cadastro e login', () => {
 
-    /* 
-    Testes End To End ou Testes de ponta a ponta, ligam uma série de funcionalidades de um sistema,
-    simulando o comportamento do usuário final. Esses testes verificam se diferentes partes do sistema
-    funcionam corretamente quando integradas, garantindo que o fluxo completo de uma funcionalidade
-    funcione como esperado.
-    Aqui iremos criar um teste end to end que cobre o fluxo de cadastro e login de um usuário em um sistema web.
-    Em apenas um teste, ou seja, em um único "it", iremos:
-    1. Acessar a página de cadastro.
-    2. Preencher o formulário de cadastro com dados válidos.
-    3. Submeter o formulário e verificar se o cadastro foi bem-sucedido.
-    4. Acessar a página de login.
-    5. Preencher o formulário de login com as credenciais do usuário recém-cadastrado.
-    6. Submeter o formulário de login e verificar se o login foi bem-sucedido.
-
-    Use as boas práticas aprendidas até agora para estruturar o teste.
-    */
-
-    beforeEach(() => {
-        // Configurações iniciais, se necessário
+    afterEach(() => {
+        cy.screenshot()
     });
 
+    it('Deve realizar o cadastro e, em seguida, o login com sucesso', () => {
+        // 1. Criação da Massa de Dados 
+        let emailDinamico = `teste${Date.now()}@teste.com`
+        let nomeDinamico = faker.person.fullName()
+        let senhaE2E = 'Teste@123'
 
-    it('Deve fazer o cadastro e validar o login com o usuário cadastrado', () => {
-        // Criar todo o fluxo aqui dentro deste único "it"
+        // 2. ETAPA DE CADASTRO
+        cy.visit('register.html')
+        
+        cy.preencherCadastro(
+            nomeDinamico,
+            emailDinamico,
+            '31987654321',
+            senhaE2E,
+            senhaE2E
+        ) 
+
+        // 3. ETAPA DE LOGIN
+        cy.visit('login.html') 
+        cy.login(emailDinamico, senhaE2E)
+        
     });
 });
